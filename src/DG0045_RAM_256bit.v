45b9652c5acff798cdc375469e2a4d4dc870691b
OliverSmithBH2VGM <160381698+OliverSmithBH2VGM@users.noreply.github.com>
2024-02-18 23:19:31 +0800

    Update DG0045_RAM_256bit.v

--- src/DG0045_RAM_256bit.v
+++ src/DG0045_RAM_256bit.v
@@ -5,13 +5,13 @@ module DG0045_RAM_256bit(
            output wire[3:0]dout
           );
  
-reg [3:0] mem [0:63];
- 
+//reg [3:0] mem [0:63];
+           reg [3:0] mem [0:15];
 always @(posedge RAM_clk)
 begin
-        mem [addr] <= din;
+        mem [addr[3:0]] <= din;
 end
 
-assign   dout = mem [addr] ;
+           assign   dout = mem [addr[3:0]] ;
 
 endmodule
