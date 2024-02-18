c41aa58090f2469e4074fab10faf9f183bc8df17
OliverSmithBH2VGM <160381698+OliverSmithBH2VGM@users.noreply.github.com>
2024-02-18 21:51:48 +0800

    Create DG0045_RAM_256bit.v

---
+++ src/DG0045_RAM_256bit.v
@@ -0,0 +1,17 @@
+module DG0045_RAM_256bit(
+           input   wire RAM_clk  ,
+           input   wire[5:0] addr ,
+           input   wire[3:0]din  ,
+           output wire[3:0]dout
+          );
+ 
+reg [3:0] mem [0:63];
+ 
+always @(posedge RAM_clk)
+begin
+        mem [addr] <= din;
+end
+
+assign   dout = mem [addr] ;
+
+endmodule
