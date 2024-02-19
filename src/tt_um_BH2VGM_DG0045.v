1ae2f89f3ecb44a08b096f1c0ce29f54eb277c5a
OliverSmithBH2VGM <160381698+OliverSmithBH2VGM@users.noreply.github.com>
2024-02-19 14:06:23 +0800

    Update tt_um_BH2VGM_DG0045.v

--- src/tt_um_BH2VGM_DG0045.v
+++ src/tt_um_BH2VGM_DG0045.v
@@ -5,30 +5,6 @@
 
 `define default_netname none
 
-/*module tt_um_BH2VGM_DG0045(
-	input wire clk,  // main clock  // posedge effective // 8 posedge = 1 machine cycle
-	input wire rst_n, // reset signal // low effective
-	input wire ena, // if ena = 1, CPU works, else, clk has no effect
-
-	input wire[7:0] ui_in,  // connects to mainROM
-	output wire[7:0] uo_out, // PC_HL[4:0] outs and nL[3:2] and ND
-	input wire[7:0] uio_in, //KIN at uio_in[3:0], PC_MUX at uio_in[5]
-	output wire[7:0] uio_out,// nL outs at uio_out[7:6]
-	output wire[7:0] uio_oe// bit 0 means input ; bit 1 means output
-);
-
-	reg[7:0] outregs;
-
-	always@(posedge clk or negedge rst_n)
-	begin
-		if(rst_n==1'b0)begin outregs <= 8'b00000000  ; end
-		else begin outregs <= ena? uio_in:ui_in; end
-	end
-	assign uo_out = outregs;
-	assign uio_oe = 8'b00001111;
-	assign uio_out = 8'b00001111;
-endmodule*/
-
 module tt_um_BH2VGM_DG0045(
 	input wire clk,  // main clock  // posedge effective // 8 posedge = 1 machine cycle
 	input wire rst_n, // reset signal // low effective
@@ -75,7 +51,7 @@ wire RZ;
 wire SMP;
 wire DECB;
 wire SZ;
-wire RET;wire RETSK;
+wire RET;
 
 wire SKMP;
 wire SHD;
@@ -119,13 +95,13 @@ reg[7:0] nowCMD;
   	assign DECB=(nowCMD == 8'b01011100) ?1'b1:1'b0;
 
   	assign SZ=(nowCMD == 8'b01011101) ?1'b1:1'b0;
- 	assign RET =(nowCMD[7:1] == 8'b0101111) ?1'b1:1'b0;
+ 	assign RET =(nowCMD[7:1] == 7'b0101111) ?1'b1:1'b0;
 
 //		 (nowCMD[7:2] == 6'b011000)?LBS:
   	assign SKMP=(nowCMD[7:2] == 6'b011001)?1'b1:1'b0;
 //		 (nowCMD == 8'b01101000)?ResetW:
 //		 (nowCMD == 8'b01101001)?SetW:
- 	assign  SHD= (nowCMD[7:1] == 8'b0110101)?1'b1:1'b0;
+ 	assign  SHD= (nowCMD[7:1] == 7'b0110101)?1'b1:1'b0;
 //		 (nowCMD == 8'b01101100)?LTSPU:
   	assign ATL=(nowCMD == 8'b01101101)?1'b1:1'b0;
  	assign RNP= (nowCMD == 8'b01101110)?1'b1:1'b0;
@@ -158,7 +134,7 @@ assign CLKF2 = (clock_divider[2:1] == 2'b11)? 1'b1:1'b0;
 
 reg CLKEN;
 wire F1,F2;
-always@(negedge CLKF2)
+always@(negedge CLKF2 or negedge RESET)
 begin
 	if(RESET == 1'b0)begin
 		CLKEN <= 1'b0;
@@ -504,6 +480,7 @@ assign nL = ~Lreg;
 //****G 
 
 endmodule
+
 /*
 module DG0045_RAM_256bit(
            input   wire RAM_clk  ,
