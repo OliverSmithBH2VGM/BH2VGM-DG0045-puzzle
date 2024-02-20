62b79b0bd5e2572535a462098ab3839d6ae90335
OliverSmithBH2VGM <160381698+OliverSmithBH2VGM@users.noreply.github.com>
2024-02-20 11:34:25 +0800

    Update and rename DG0045_RAM_128bit.v to DG0045_RAM_256bit.v

--- src/DG0045_RAM_256bit.v
+++ src/DG0045_RAM_256bit.v
@@ -14,14 +14,14 @@ end
            assign   dout = mem [addr] ;
 
 endmodule*/
-module DG0045_RAM_128bit(
+module DG0045_RAM_256bit(
            input   wire RAM_clk  ,
-           input   wire[4:0] addr ,
+           input   wire[5:0] addr ,
            input   wire[3:0]din  ,
            output wire[3:0]dout
           );
  
-reg [3:0] mem [0:31];
+           reg [3:0] mem [0:63];
  
 always @(posedge RAM_clk)
 begin
